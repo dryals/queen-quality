d01c1c4c9fc68057c94867b52285995aa22322a1
Dylan Ryals <dylan.k.ryals@gmail.com>
2026-02-12 14:57:39 -0500

    blup

--- pheno_adjust.R
+++ pheno_adjust.R
@@ -123,11 +123,11 @@ preblup = preblup %>%
               
 #read grm
   
-  G = read.delim("/scratch/negishi/dryals/queen-quality/plink/samples-filter.king", 
+  G = read.delim("/scratch/negishi/dryals/queen-quality/plink/samples-filter.rel", 
   sep = "", header = F) %>% as.matrix()
   
-  Gid = read.delim("/scratch/negishi/dryals/queen-quality/plink/samples-filter.king.id", 
-  sep = "", header = T)
+  Gid = read.delim("/scratch/negishi/dryals/queen-quality/plink/samples-filter.rel.id", 
+  sep = "", header = F)
   
   colnames(G) = rownames(G) = Gid[,1]
   
