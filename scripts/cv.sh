7d123d5192fe732c7b40055ab346b1db047c53de
Dylan Ryals <dylan.k.ryals@gmail.com>
2026-02-13 14:29:56 -0500

    blup

--- scripts/cv.sh
+++ scripts/cv.sh
@@ -4,7 +4,7 @@ cd ~/ryals/queen-quality/blup
 
 echo "starting blup cv run $2 / 5"
 
-./blupf90+ ${1}.par1 &> cv-lastrun.log
+./blupf90+ ${1} &> cv-lastrun.log
 
 mv solutions ../data/sol-cv${2}.txt
 
